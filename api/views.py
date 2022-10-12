b73b4a985790c8931ba1312e086eebcd302602e6
CalGordonHallCounty <cgordon@hallcounty.org>
2022-10-12 18:18:23 -0400

    Initial commit

---
+++ api/views.py
@@ -0,0 +1,23 @@
+
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+from rest_framework import permissions, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer, UserSerializerWithToken
+
+@api_view(['GET'])
+def current_user(request):
+
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
+class UserList(APIView):
+    permission_classes = (permissions.AllowAny,)
+    def post(self, request, format=None):
+        serializer = UserSerializerWithToken(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
