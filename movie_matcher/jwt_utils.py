665cb1126ccbfcca17fed5a51f7310f1e99579da
Cal <cgordon2@ggc.edu>
2022-10-12 18:28:11 -0400

    Initial Commit

--- movie_matcher/jwt_utils.py
+++ movie_matcher/jwt_utils.py
@@ -1,8 +1,8 @@
-
 from api.serializers import UserSerializer
-def my_jwt_response_handler(token, user=None, request=None):
+
+
+def my_jwt_response_handler(token, user=None, request=None, extra=None):
     return {
         'token': token,
         'user': UserSerializer(user, context={'request': request}).data
     }
-    
