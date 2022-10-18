26bab29de02a423620d8813e88f0bc0f1e3641c9
Jeremy Stiff <jstiff1092@gmail.com>
2022-10-18 16:48:22 -0400

    Raw infinite scrolling

--- app/src/components/Nav.js
+++ app/src/components/Nav.js
@@ -4,22 +4,23 @@ import PropTypes from 'prop-types';
 function Nav(props) {
   const logged_out_nav = (
     <ul>
-      <li onClick={() => props.display_form('login')}>login</li>
-      <li onClick={() => props.display_form('signup')}>signup</li>
-        <li onClick={() => props.display_form('movie')}>movie</li>
+        <li onClick={() => props.display_form('login')}>login</li>
+        <li onClick={() => props.display_form('signup')}>signup</li>
+        <li onClick={() => props.display_form('library')}>library</li>
     </ul>
   );
   const logged_in_nav = (
     <ul>
-      <li onClick={props.handle_logout}>logout</li>
+        <li onClick={props.handle_logout}>logout</li>
+        <li onClick={() => props.display_form('library')}>library</li>
     </ul>
   );
   return <div>{props.logged_in ? logged_in_nav : logged_out_nav}</div>;
 }
 export default Nav;
 Nav.propTypes = {
-  logged_in: PropTypes.bool.isRequired,
-  display_form: PropTypes.func.isRequired,
-  handle_logout: PropTypes.func.isRequired
+    logged_in: PropTypes.bool.isRequired,
+    display_form: PropTypes.func.isRequired,
+    handle_logout: PropTypes.func.isRequired
 };
     
