624633d2758dc42e8fda030a8f96685b944bf5fe
Jeremy Stiff <jstiff1092@gmail.com>
2022-10-18 19:53:22 -0400

    It was simple CSS :((((((((

--- app/src/components/InfiniteScrolling.js
+++ app/src/components/InfiniteScrolling.js
@@ -7,7 +7,7 @@ class InfiniteScrolling extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            items: [],
+            items: [[]],
             query: props.query,
             pageNumber: 2
         };
@@ -37,6 +37,27 @@ class InfiniteScrolling extends React.Component {
         console.log(this.state.items);
     }
 
+    solutionForTable = array => {
+        const out = [];
+        let inner = [];
+        if (array[array.length - 1].length !== 3 && array[0].length !== 0) {
+            const pile = array.pop();
+            out.push(...array);
+            for (let i in pile)
+                inner.push(i);
+        }
+        for (let i in array) {
+            if (inner.length === 3) {
+                out.push(inner);
+                inner = [];
+            }
+            inner.push(i);
+        }
+        if (inner.length !== 0)
+            out.push(inner);
+        return out;
+    }
+
     render() {
         return (
             <InfiniteScroll
@@ -45,17 +66,19 @@ class InfiniteScrolling extends React.Component {
                 loader={<h4>Loading more...</h4>}
                 dataLength={this.state.items.length}
             >
+                <div style={{display: "flex", flexWrap: "wrap"}}>
                 {this.state.items.map((i) => {
                     return (
-                    <div key={i.id}>
-                        <img key={i['poster_path']} alt={`A poster for ${i.title}`} src={`https://image.tmdb.org/t/p/w500${i['backdrop_path']}`}/>
-                        <div>{i.title}</div>
-                        <div>{i['vote_average']}</div>
-                        <div>{i['release_date']}</div>
-                        <br/>
-                    </div>
-                )
-                })}
+                        <div key={i.id} style={{margin: 5}}>
+                            <img key={i['poster_path']} alt={`A poster for ${i.title}`}
+                                 src={`https://image.tmdb.org/t/p/w500${i['backdrop_path']}`}/>
+                            <div>{i.title}</div>
+                            <div>{i['vote_average']}</div>
+                            <div>{i['release_date']}</div>
+                            <br/>
+                        </div>
+                    )
+                })}</div>
             </InfiniteScroll>
         );
     }
