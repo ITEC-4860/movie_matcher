d7537e8fa1568db7993619aeaf20ab4d35a06e7f
jshan98 <jshanahan@ggc.edu>
2022-10-31 15:16:35 -0400

    Added buttons, genres, and runtime to popups

--- app/src/components/MoviePage.js
+++ app/src/components/MoviePage.js
@@ -11,13 +11,21 @@ class MoviePage extends React.Component {
             query: props.query
         }
         this.state = {
-            items: []
+            items: [],
+            genres: [],
         };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(){
+        alert( this.state.items.title + " was added to INSERT_LIST_TITLE_HERE");
     }
 
     componentDidMount() {
         movieById(this.props.query).then((res) => {
             this.updateState('items', res.data);
+            this.updateState('genres', res.data['genres']);
         });
     }
 
@@ -65,8 +73,22 @@ class MoviePage extends React.Component {
         if (prevProps.query !== this.props.query) {
             movieById(this.props.query).then((res) => {
                 this.updateState('items', res.data);
+                this.updateState('genres', res.data['genres']);
             });
         }
+    }
+
+    getGenres(){
+        let allGenres = "";
+        for(let i = 0; i < this.state.genres.length; i++){
+            allGenres += this.state.genres.at(i).name + ", ";
+        }
+        return "Genres: " + allGenres.substring(0, allGenres.length-2);
+    }
+
+    getRuntime() {
+        let runtime = this.state.items['runtime'] / 60;
+        return "Runtime: " + runtime.toString().at(0) + " Hour(s) & " + runtime.toString().at(2) + "0 Minute(s)"
     }
 
     render() {
@@ -79,8 +101,11 @@ class MoviePage extends React.Component {
                              src={`https://image.tmdb.org/t/p/w500${this.state.items['poster_path']}`}/>
                         <div className={'col order-1 col-sm-8'}>
                             <h1 style={{fontWeight: "bold"}}>{this.state.items.title}</h1>
-                            <h6> Released: {this.state.items['release_date']}</h6>
+                            <h6> Released: {this.state.items['release_date'] + "\n" + this.getGenres() + "\n" + this.getRuntime()}</h6>
                             <p style={{fontSize: "12pt"}}>{this.state.items['overview']}</p>
+                        </div>
+                        <div className={'col order-2 col-sm-8'}>
+                            <button onClick={this.handleClick}> Add to List </button>
                         </div>
                         <br/>
                     </div>
