b9a684950cb0d499539eb7e938496a83418fa525
jshan98 <jshanahan@ggc.edu>
2022-11-04 15:40:29 -0400

    Applied boostrap Navbar class to navbar and added basic styling

--- app/src/components/Nav.js
+++ app/src/components/Nav.js
@@ -1,19 +1,21 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../Nav.css';
 function Nav(props) {
   const logged_out_nav = (
-    <ul>
-      <li onClick={() => props.display_form('login')}>login</li>
-      <li onClick={() => props.display_form('signup')}>signup</li>
+    <ul className={"navbar-nav mr-auto"}>
+      <li className={"nav-item"} onClick={() => props.display_form('login')}>Login</li>
+      <li className={"nav-item"} onClick={() => props.display_form('signup')}>SignUp</li>
     </ul>
   );
   const logged_in_nav = (
-    <ul>
-      <li onClick={props.handle_logout}>logout</li>
+    <ul className={"navbar-nav mr-auto"}>
+      <li className={"nav-item"} onClick={props.handle_logout}>logout</li>
     </ul>
   );
-  return <div>{props.logged_in ? logged_in_nav : logged_out_nav}</div>;
+  return <div className={"navbar navbar-expand-lg navbar-dark bg-dark text-light"}>{props.logged_in ? logged_in_nav : logged_out_nav}</div>;
 }
 export default Nav;
 Nav.propTypes = {
