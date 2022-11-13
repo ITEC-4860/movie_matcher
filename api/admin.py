a96b9eaa75c42eb691c0ae6ef390fad6effc0136
Cal <cgordon2@ggc.edu>
2022-11-12 19:11:31 -0500

    Profiles now correctly create and link to user accounts when new sign-ups occur

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from api.models import Profile
+
+
 # Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'phone', 'address', 'city', 'state', 'zip')
+        }),
+
+    )
+
+
+admin.site.register(Profile, ProfileAdmin)
